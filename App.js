d6898d3d809e8ccd62264d70f21e33881efb5b34
David Cisneros <Dcisneros101@gmail.com>
2019-05-20 14:09:33 -0600

    Scrap native base

--- App.js
+++ App.js
@@ -1,43 +1,14 @@
-import { Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
 import React, { Component } from 'react';
-import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+import GeneralStatusBarColor from './src/Components/Styles/GeneralStatusBarColor';
+import { View } from 'react-native';
 export default class App extends Component {
-  state = {
-    fontLoaded: false,
-  };
-  
-  async componentDidMount() {
-    await Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-
-    this.setState({ fontLoaded: true });
-  }
 
   render() {
     return (
-      <Container>
-        { this.state.fontLoaded === true && 
-        <Header>
-        <Left>
-          <Button transparent>
-            <Icon name='arrow-back' />
-          </Button>
-        </Left>
-        <Body>
-          <Title>Get Swole</Title>
-        </Body>
-        <Right>
-          <Button transparent>
-            <Icon name='menu' />
-          </Button>
-        </Right>
-      </Header>
-      }
-      </Container>
+      <View style={{ flex: 1 }}>
+        <GeneralStatusBarColor backgroundColor="#7CABCC"
+          barStyle="light-content" />
+      </View>
     );
   }
 }
