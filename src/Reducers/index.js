15b42ec6065be80876948226994d97b05ad1758d
Jake <jakeadmire1@gmail.com>
2019-05-25 10:50:02 -0600

    Add loading.gif image

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,5 +1,6 @@
 import { userReducer } from './userReducer';
 import { dateReducer } from './dateReducer';
+import { semanticDateReducer } from './semanticDateReducer';
 import { exercisesReducer } from './exercisesReducer';
 import { routinesReducer } from './routinesReducer';
 import { loadingReducer } from './loadingReducer';
@@ -9,6 +10,7 @@ import { combineReducers } from 'redux';
 export const rootReducer = combineReducers({
   user: userReducer,
   date: dateReducer,
+  semanticDate: semanticDateReducer,
   exercises: exercisesReducer,
   routines: routinesReducer,
   loading: loadingReducer
