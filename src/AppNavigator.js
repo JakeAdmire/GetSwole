663943e6dda4c7ca30f25d2febf7b9d99e42ce9c
Jake <jakeadmire1@gmail.com>
2019-05-28 16:38:40 -0600

    Refactor AppNavigator & add link to routineDetails page
    Co-authored-by: <tnelsallen3@gmail.com>

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,26 +1,24 @@
 import { createStackNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation';
 import WelcomePage from './Components/WelcomePage/WelcomePage';
 import HomePage from './Components/HomePage/HomePage';
-import RoutineContainer from './Components/RoutineContainer/RoutineContainer'
+import RoutineContainer from './Components/RoutineContainer/RoutineContainer';
+import RoutineDetails from './Components/RoutineDetails/RoutineDetails';
+
 
 const rootStack = createStackNavigator({
   homePage:  { screen: HomePage },
-  routinePage:  { screen: RoutineContainer }
-  },
-  {
-    initialRouteName: "homePage"
-  }
-);
+  routinePage:  { screen: RoutineContainer },
+  routineDetails: { screen: RoutineDetails }
+
+}, { initialRouteName: "homePage" })
 
 const AppNavigator = createAppContainer(
+
   createSwitchNavigator({
     welcomePage: WelcomePage,
     MainApp: rootStack
-  },
-  {
-    initialRouteName: "welcomePage"
-  }
-  )
-);
+  }, { initialRouteName: 'welcomePage' })
+
+)
 
 export default AppNavigator;
