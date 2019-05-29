92e1b3db610e814674b0ad07a07ef6fa45f35590
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-29 01:24:42 -0600

    Add error test in addUserThunk

--- src/Thunks/__tests__/addUserThunk.test.js
+++ src/Thunks/__tests__/addUserThunk.test.js
@@ -11,9 +11,20 @@ describe('addUserThunk', () => {
     mockDispatch = jest.fn()
   })
 
-  it('should dispatch isLoading(true)', async () => {
+  it('should dispatch isLoading(true)', () => {
     const thunk = addUserThunk(mockUser.name)
-    await thunk(mockDispatch)
+    thunk(mockDispatch)
     expect(mockDispatch).toHaveBeenCalledWith(actions.isLoading(true))
   })
+
+  it('should dispatch hasError with a message if the response is not OK', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: false,
+      statusText: 'Something went wrong.'
+    }))
+    const thunk = addUserThunk(mockUser.name)
+    await thunk(mockDispatch)
+    expect(mockDispatch).toHaveBeenCalledWith(actions.hasError('Something went wrong.'))
+  })
+
 })
